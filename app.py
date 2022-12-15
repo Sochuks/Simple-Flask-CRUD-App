de27d275b6de834fedb5796ab356cc7932bdebbe
Sochuks <moseschukwumakajang@gmail.com>
2022-12-15 04:24:08 +0100

    -Modified app imported psycopg2, psycopg2.extras - created variables to hold postgresql info -Created database testdb - created connection using details.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
+app.secret_key = 'crud-flask-students'
+
+DB_HOST = 'localhost'
+DB_NAME = 'testdb'
+DB_USER = 'postgres'
+DB_PASS = 'guest@dmin'
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, passsword=DB_PASS, host=DB_HOST)
+
 
 @app.route('/')
 
