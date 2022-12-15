fa7b3696f03f35136ade2ec255f48b9017e94f28
Sochuks <moseschukwumakajang@gmail.com>
2022-12-15 17:18:53 +0100

    -Modified app added add_student to route -Created a function add_student -Established cur connection -get form content -execute INSERT to students table using form contents - commit and display flash message before redirect -Modified index added flash message set formto POST and ACTION to add_student

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 import psycopg2
 import psycopg2.extras
 
@@ -17,10 +17,25 @@ conn = psycopg2.connect(host=DB_HOST, user=DB_USER, password=DB_PASS, database=D
 
 def Index():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    s = "SELECT*FROM student"
+    s = "SELECT*FROM students"
     cur.execute(s) # Execute SQL
     list_user = cur.fetchall()
     return render_template('index.html', list_user=list_user)
+
+@app.route('/add_student', methods={"POST"})
+
+def add_student():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        cur.execute("INSERT INTO students (fname, lname, email) VALUES (%s,%s,%s)", (fname, lname, email))
+        conn.commit()
+        flash ('User added successfuly')
+        return redirect(url_for('Index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
